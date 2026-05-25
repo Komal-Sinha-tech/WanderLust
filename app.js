ee22b0462667fb9566751be98185a466f1fb667a
Komal Sinha <sinhakomal589@gmail.com>
2026-05-26 00:00:32 +0530

    validation middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingschema} = require("./schema.js");
 
 //connection to DB
 const MONGO_URL ="mongodb://127.0.0.1:27017/WanderLust";
@@ -30,6 +31,15 @@ app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
+const validateListing = (req,res,next)=>{
+    let {error} = listingschema.validate(req.body);
+   let errMsg = error.details.map((el)=>el.message).join(",");
+   if(error){
+    throw new ExpressError(400,errMsg);
+   }else{
+    next();
+   }
+}
 //root route
 app.get("/",(req,res)=>{
     res.send("hi, I am root");
@@ -73,14 +83,14 @@ app.get("/listings/:id", wrapAsync(async(req,res)=>{
 }));
 
 //Create Route
-app.post("/listings", wrapAsync(async(req,res,next)=>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"send valid data for listing");
-    }
+app.post("/listings", validateListing,wrapAsync(async(req,res,next)=>{
+   let result = listingschema.validate(req.body);
+   console.log(result);
+   if(result.error){
+    throw new ExpressError(400,result.error);
+   }
     const newListing = new Listing (req.body.listing);
-    if(!req.body.listing){
-        throw new ExpressError(400,"send valid data for listing");
-    }
+
     await newListing.save();
     res.redirect("/listings");
 }));
@@ -94,14 +104,11 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-    if(!req.body.listing){
-        throw new ExpressError(400,"send valid data for listing");
-    }
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing });
   res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
 app.delete("/listings/:id",async(req,res)=>{
