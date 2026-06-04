f0025b8e4ddb3b2a220252d5ef7a9accba9998b0
Komal Sinha <sinhakomal589@gmail.com>
2026-06-04 23:45:03 +0530

    authenticate passport use

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//Require all packages
+// Require all packages
 const express = require("express");
-const session = require ("express-session");
+const session = require("express-session");
 const flash = require("connect-flash");
 const app = express();
 const mongoose = require("mongoose");
@@ -8,77 +8,108 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
 
-//routes
+const ExpressError = require("./utils/ExpressError");
+
+const User = require("./models/user.js");
+
+// Routes
 const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 
-//connection to DB
-const MONGO_URL ="mongodb://127.0.0.1:27017/WanderLust";
+// Connection to DB
+const MONGO_URL = "mongodb://127.0.0.1:27017/WanderLust";
+
 main()
-.then(()=>{
+.then(() => {
     console.log("connected to DB");
 })
-.catch((err)=>{
+.catch((err) => {
     console.log(err);
-})
-async function main(){
-    await mongoose.connect(MONGO_URL);
-};
+});
 
-//middlewares
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+// Middlewares
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
 
+app.engine("ejs", ejsMate);
 
+app.use(express.static(path.join(__dirname, "/public")));
 
-//session
+// Session
 const sessionOptions = {
-    secret:"secretCode",
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        express:Date.now() + 7 * 24 * 60 *60 * 100,
-        maxAge: 7 * 24 * 60 * 60 * 100,
-        httpOnly :true,
+    secret: "secretCode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
     },
 };
 
-//root route
-app.get("/",(req,res)=>{
-    res.send("hi, I am root");
-});
-
 app.use(session(sessionOptions));
-  app.use(flash());
+app.use(flash());
 
-//session connect
+// Passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Flash middleware
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 });
 
-//routes connect
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
+// Root route
+app.get("/", (req, res) => {
+    res.send("hi, I am root");
+});
 
-//Error
+// Demo user route
+app.get("/demouser", async (req, res) => {
+
+    let fakeUser = new User({
+        email: "student@gmail.com",
+        username: "student",
+    });
+
+    let registeredUser = await User.register(fakeUser, "helloWorld");
+
+    res.send(registeredUser);
+});
+
+// Routes connect
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+
+// Error
 app.use((req, res, next) => {
     next(new ExpressError(404, "Page Not Found"));
 });
 
-app.use((err,req,res,next)=>{
-    let{status=500,message="something went wrong"}= err;
-   res.status(status).render("listings/error.ejs", { err });
-    // res.status(status).send(message);
+app.use((err, req, res, next) => {
+    let { status = 500, message = "something went wrong" } = err;
+
+    res.status(status).render("listings/error.ejs", { err });
 });
 
-//Server confirmation
-app.listen(8080,()=>{
+// Server confirmation
+app.listen(8080, () => {
     console.log("server is running on port 8080");
 });
