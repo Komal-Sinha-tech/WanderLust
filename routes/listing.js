92feb4097ddf9c5180b42c50d8ce358e21ae5950
Komal Sinha <sinhakomal589@gmail.com>
2026-06-14 05:48:06 +0530

    form capability of sending files to backend using multer

--- routes/listing.js
+++ routes/listing.js
@@ -6,13 +6,19 @@ const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 
 router
 .route("/")
 //Index Route
 .get(wrapAsync(listingController.index))
 //Create Route
-.post(isLoggedIn, validateListing,wrapAsync(listingController.createListing));
+// .post(isLoggedIn, validateListing,wrapAsync(listingController.createListing));
+.post( upload.single('listing[image]'),(req, res)=>{
+    res.send(req.file);
+})
 
 //New Route
 router.get("/new",isLoggedIn,listingController.renderNewForm);
