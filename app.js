e3ac10e35b3a96e888a9df049eca02a246451655
Komal Sinha <sinhakomal589@gmail.com>
2026-06-19 01:40:14 +0530

    Add root route

--- app.js
+++ app.js
@@ -105,6 +105,10 @@ app.use((req, res, next) => {
 //     res.send(registeredUser);
 // });
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 // Routes connect
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
